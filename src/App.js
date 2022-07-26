fde9f876a7d3adeb5078a2e01c6f08a645ea2290
abnerrivera <abner500edrey@gmail.com>
2022-07-26 15:15:52 -0500

    create a header of  home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
     </div>
   );
 }
