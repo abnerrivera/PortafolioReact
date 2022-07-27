f1da5a97b37d705c6a8f5d473548f6eeaf548a5c
abnerrivera <abner500edrey@gmail.com>
2022-07-26 22:21:29 -0500

    change general gradient and add new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import Experience from "./components/Experience/Experience";
 import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
+import Portfolio from "./components/Portfolio/Portfolio";
+import SocialLinks from "./components/SocialLinks/SocialLinks";
 
 function App() {
   return (
-    <div>
+    <div className="bg-gradient-to-b from-black to-gray-900">
       <NavBar />
       <Home />
+      <Portfolio />
+      <Experience />
+
+
+      <SocialLinks />
     </div>
   );
 }
