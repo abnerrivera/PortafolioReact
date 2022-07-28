e4008808b51f8a55cc89ba7770dee5c3b12fcf0b
abnerrivera <abner500edrey@gmail.com>
2022-07-27 23:15:54 -0500

    add portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Portfolio.css'
-import imgPortfolio from '../../assets/portfolio/arrayDestruct.jpg'
+
+import countriesApi from '../../assets/portfolio/countriesApi.png'
+import portfolio from '../../assets/portfolio/portfolio.png'
+import bEcommerce1 from '../../assets/portfolio/bEcommerce1.png'
 
 
 const Portfolio = () => {
@@ -8,28 +11,23 @@ const Portfolio = () => {
   const projects = [
     {
       id: 1,
-      src: imgPortfolio,
-      demo: '',
-      code: ''
+      src: countriesApi,
+      demo: 'https://countries-api-react-abnerdev.vercel.app/',
+      code: 'https://github.com/abnerrivera/CountriesApiReact'
     },
     {
       id: 2,
-      src: imgPortfolio,
-      demo: '',
-      code: ''
+      src: portfolio,
+      demo: 'https://portfolio-abnerdev.vercel.app/',
+      code: 'https://github.com/abnerrivera/PortafolioReact'
     },
     {
       id: 3,
-      src: imgPortfolio,
-      demo: '',
-      code: ''
+      src: bEcommerce1,
+      demo: 'https://github.com/abnerrivera/BackendEcommerceNodejs',
+      code: 'https://github.com/abnerrivera/BackendEcommerceNodejs'
     },
-    {
-      id: 4,
-      src: imgPortfolio,
-      demo: '',
-      code: ''
-    },
+
   ]
 
   return (
@@ -37,7 +35,7 @@ const Portfolio = () => {
       name="portfolio"
       className="mt-16 w-full text-white md:h-screen"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
@@ -46,20 +44,19 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {projects.map(({ id, src }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+          {projects.map(({ id, src, code, demo }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg duration-200 hover:scale-110">
               <img
                 src={src}
                 alt=""
-                className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a target='_blank' rel='noreferrer' href={demo} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                </a>
+                <a target='_blank' rel='noreferrer' href={code} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Code
-                </button>
+                </a>
               </div>
             </div>
           ))}
